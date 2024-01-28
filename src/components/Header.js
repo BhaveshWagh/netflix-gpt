e8c07fa4f21f0cce3fac7bd22bc24866b01d7a3b
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-01-28 15:00:44 +0530

    Big fixed: user not logged In redirect /browse to login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,63 @@
-import { LOGO, USER_AVATAR } from "../utils/constants";
-import { signOut } from "firebase/auth";
+import { LOGO, PHOTO_URL, USER_AVATAR } from "../utils/constants";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const user = useSelector((store) => store.user);
 
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-
-        navigate("/");
-      })
+      .then(() => {})
       .catch((error) => {
-        // An error happened.
         navigate("/error");
       });
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    // I want to unsubscribe whenever my component unmounts
+    // unsubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" alt="logo" src={LOGO} />
 
       {user && (
         <div className="flex p-2 gap-2 ">
-          <p className="text-white text-2xl font-semibold py-2">{user.displayName}</p>
-          <img className="w-12 h-12 " alt="user" src={user?.photoURL} />
+          <p className="text--500 text-2xl font-semibold py-2">
+            {user.displayName}
+          </p>
+          <img
+            className="w-12 h-12 "
+            alt="user"
+            src={user?.photoURL}
+          />
           <button
             onClick={handleSignOut}
             className="font-bold p-2 h-12 bg-red-600 text-white rounded-lg "
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const USER_AVATAR =
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const PHOTO_URL = "https://avatars.githubusercontent.com/u/62753935?v=4";
