fe6b6edfd93712ad8e50219da8db08a30e3af203
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-02-02 15:28:01 +0530

    Multi Language Support in GPT Search Page

---
+++ src/utils/languageConstant.js
@@ -0,0 +1,20 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceHolder: "What would you like to watch today ?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे ?",
+  },
+  marathi: {
+    search: "शोध",
+    gptSearchPlaceHolder: "तुम्हाला आज काय बघायला आवडेल ?",
+  },
+  german: {
+    search: "Suchen",
+    gptSearchPlaceHolder: "Was möchtest du heute sehen ?",
+  },
+};
+
+export default lang;
